f3f4951d8229afc03b362d72050ffcacb7898b26
Kasey <mail.kaseyc@gmail.com>
2021-10-25 19:52:01 -0500

    view

---
+++ src/components/list.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class TodoList extends Component {
+    render(){
+        return (
+            <div>
+                <p>This is the to-do list component</p>
+            </div>
+        )
+    }
+}
